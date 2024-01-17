2b94dcffe798c2ab58813656761bce66427bb5a6
Mojtaba <m.ghaderi2166@gmail.com>
2024-01-17 11:26:20 +0330

    Just came back to this project after a while

--- DigitalStore/settings.py
+++ DigitalStore/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'DigitalStore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'eshop',
+        'USER': 'moji',
+        'PASSWORD': 'moji_is_great',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- ProductCatalog/models.py
+++ ProductCatalog/models.py
@@ -1,4 +1,11 @@
 from django.db import models
 
 
+class CategoryModel(models.Model):
+    category = models.CharField(unique=True, max_length=100)
+    parent = models.CharField(null=True, max_length=100)
+    child = models.CharField(null=True, max_length=100)
 
+
+class ProductModel(models.Model):
+    pass
