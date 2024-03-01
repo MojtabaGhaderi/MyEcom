dbf0b8b2e42cd33e6bb392f03e3e04816d6a3b9a
Mojtaba <m.ghaderi2166@gmail.com>
2024-03-01 20:54:56 +0330

    .

--- ShoppingCart/views.py
+++ ShoppingCart/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import render
-from rest_framework import status, generics, viewsets
-from rest_framework.decorators import api_view
+from rest_framework import status, generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
